da5b2d750f171e2d011c27b07879f41c68a909e1
hjm <2445543801@qq.com>
2018-12-16 15:04:50 +0800

    完成用户管理页面

--- src/api/index.js
+++ src/api/index.js
@@ -4,6 +4,9 @@ const baseURL ='http://127.0.0.1:3000'
 
 axios.defaults.baseURL = baseURL
 
+// axios的访问请求默认是不带cookie的，因此想要带cookie的请求，需要设置一个参数
+axios.defaults.withCredentials = true
+
 // 登录请求
 export const Login = (params) => {
     return axios.post('/employee/employeeLogin',params)
@@ -15,6 +18,14 @@ export const Login = (params) => {
 // 用户管理
 export const UserList = (params) => {
     return axios.get('/user/queryUser',params)
+    .then((result) => {
+        return result.data
+    })
+}
+
+// 用户管理
+export const UserStatus = (params) => {
+    return axios.post('/user/updateUser',params)
     .then((result) => {
         return result.data
     })
--- src/views/user/User.vue
+++ src/views/user/User.vue
@@ -1,67 +1,121 @@
 <template>
-    <div class="user">
-        <template>
-            <el-table
-                :data="userList"
-                border
-                style="width: 100%">
-                <el-table-column
-                type="selection"
-                width="55">
-                </el-table-column>
-                <el-table-column
-                prop="userName"
-                label="用户名"
-                width="180">
-                </el-table-column>
-                <el-table-column
-                prop="phone"
-                label="电话"
-                width="180">
-                </el-table-column>
-                <el-table-column
-                prop="state"
-                label="状态">
-                </el-table-column>
-
-                 <el-table-column label="操作">
-                    <template slot-scope="scope">
-                        <el-button
-                        size="mini"
-                        @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
-                        <el-button
-                        size="mini"
-                        type="danger"
-                        @click="handleDelete(scope.$index, scope.row)">删除</el-button>
-                    </template>
-                 </el-table-column>
-            </el-table>
+  <div class="user">
+    <template>
+      <el-table
+        :data="userList"
+        border
+        style="width: 100%"
+      >
+        <el-table-column
+          type="selection"
+          width="55"
+        >
+        </el-table-column>
+        <el-table-column
+          prop="username"
+          label="用户名"
+          width="180"
+        >
+        </el-table-column>
+        <el-table-column
+          prop="mobile"
+          label="电话"
+          width="300"
+        >
+        </el-table-column>
+        <el-table-column
+          prop="isDelete"
+          label="状态"
+        >
+        <template slot-scope='scope'>
+            <span>{{scope.row.isDelete | isDeleteForm }}</span>
         </template>
+        </el-table-column>
 
-    </div>
+        <el-table-column label="操作">
+          <template slot-scope="scope">
+            <el-button
+              size="mini"
+              type="danger"
+              @click="userStatus(scope.row.id, scope.row)"
+              :class='{"btnActive":toggleStatus===false}'
+            >{{toggleStatus?'禁用':'启用'}}</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+    </template>
+    <el-pagination
+      layout="total"
+      :total="total">
+    </el-pagination>
+  </div>
 </template>
 <script>
-import {UserList} from '@/api'
+import {UserList, UserStatus} from '@/api'
 export default{
     data(){
         return{
             userList:[
                 {
-                userName:'',
-                phone:'',
-                state:''
+                username:'',
+                mobile:'',
+                isDelete:''
             }
             ],
-            page:0,
-            pagesize:10
+            page:1,
+            pagesize:10,
+            toggleStatus:true,
+            total:0
         }
     },
     methods:{
-        handleEdit(){},
-        handleDelete(){},
+        userStatus(id,row){
+            // console.log(row,'2222');
+            this.toggleStatus = !this.toggleStatus
+            // console.log(this.toggleStatus,'123333');
+            if(this.toggleStatus){
+                this.userList[0].isDelete = 1
+                this.$message({
+                    message:'已启用',
+                    type:'success'
+                })
+            }else {
+                this.userList[0].isDelete = 0
+                this.$message({
+                    message:'已禁用',
+                    type:'error'
+                })
+            }
+            UserStatus({id:id,isDelete:row.isDelete}).then(res => {
+            })
+        },
+        initList(){
+            UserList({params:{page:this.page, pagesize:this.pagesize}}).then(res => {
+                // console.log(res,'112');
+                this.toggleStatus = !!res.rows[0].isDelete
+                this.userList = res.rows
+                this.total = res.page
+            })
+        }
+    },
+    mounted(){
+        this.initList()
+    },
+    // 拦截器
+    filters: {
+        isDeleteForm (isDelete){
+            if(isDelete === 1){
+                return '已启用'
+            }else if (isDelete === 0){
+                return '已禁用'
+            }
+        }
     }
 }
 </script>
 <style lang="scss">
-
+.btnActive{
+    background-color: skyblue;
+    border: 1px solid skyblue;
+}
 </style>
